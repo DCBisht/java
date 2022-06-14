8d7ea4c9793657df79a4e0d4dfa5345de36948e3
Divya Chakshu Bisht <chakshubisht459@gmail.com>
2022-06-14 23:04:05 +0530

    few changes

---
+++ Java programs/ne.java
@@ -0,0 +1,15 @@
+import java.util.*;
+
+class ne {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int x = -1; 
+ System.out.print(x>>>29); 
+ System.out.print(x>>>30); 
+ System.out.print(x>>>31);
+        sc.close();
+
+    }
+
+}
